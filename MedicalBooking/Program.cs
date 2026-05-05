b3e760edd90a23e029b6ac6012acee623ab7984f
Ariana Nazir <ST10442334@myemeris.edu.za>
2026-05-05 17:10:56 +0200

    Register reminder services

--- MedicalBooking/Program.cs
+++ MedicalBooking/Program.cs
@@ -1,25 +1,46 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// MVC
+builder.Services.AddControllersWithViews();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// HttpClient (API connection)
+var apiUrl = builder.Configuration["API_BASE_URL"]
+             ?? "https://localhost:7163/";
+
+builder.Services.AddHttpClient("MedicalAPI", client =>
+{
+    client.BaseAddress = new Uri(apiUrl);
+});
+
+//  AUTHENTICATION
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Auth/Login";
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
 }
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Auth}/{action=Login}/{id?}"
+);
 
 app.Run();
