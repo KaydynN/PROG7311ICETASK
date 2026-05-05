fe475f79d57118de86e5d8fdca9f0fdedfbf26c5
Jemaine08 <166508749+Jemaine08@users.noreply.github.com>
2026-05-05 16:58:28 +0200

    Add practitioner schedule model

---
+++ MedicalBooking.Web/Models/PractitionerSchedule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MedicalBooking.Web.Models
+{
+    public class PractitionerSchedule
+    {
+        public int Id { get; set; }
+        public string Practitioner { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
