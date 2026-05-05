880d1799a3130e6ac97892d72451d1578bc65bf7
Wesley Martin <ST10438473@vcconnect.edu.za>
2026-05-05 17:04:52 +0200

    Add MVC report model and controller

---
+++ MedicalBooking.Web/Controllers/ReportsController.cs
@@ -0,0 +1,30 @@
+﻿using MedicalBooking.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+[Authorize]
+public class ReportsController : Controller
+{
+    private readonly HttpClient _client;
+
+    public ReportsController(IHttpClientFactory factory)
+    {
+        _client = factory.CreateClient("MedicalAPI");
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var response = await _client.GetAsync("api/reporting/summary");
+
+        if (!response.IsSuccessStatusCode)
+            return View(new AppointmentReport());
+
+        var data = await response.Content.ReadAsStringAsync();
+
+        var report = JsonConvert.DeserializeObject<AppointmentReport>(data)
+                     ?? new AppointmentReport();
+
+        return View(report);
+    }
+}
---
+++ MedicalBooking.Web/Models/AppointmentReport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MedicalBooking.Web.Models
+{
+    public class AppointmentReport
+    {
+        public int TotalAppointments { get; set; }
+        public int AttendedCount { get; set; }
+        public int MissedCount { get; set; }
+
+        // Add PendingCount
+        public int PendingCount { get; set; }
+
+        // Add AppointmentsPerPractitioner
+        public Dictionary<string, int> AppointmentsPerPractitioner { get; set; } = new();
+    }
+}
