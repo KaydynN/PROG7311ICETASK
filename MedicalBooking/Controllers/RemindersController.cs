93ee2d18381da8295fd92d118d9420267e19c794
Ariana Nazir <ST10442334@myemeris.edu.za>
2026-05-05 17:18:25 +0200

    Add reminders MVC controller

---
+++ MedicalBooking/Controllers/RemindersController.cs
@@ -0,0 +1,44 @@
+﻿using MedicalBooking.API.Models;
+using MedicalBooking.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+[Authorize]
+public class RemindersController : Controller
+{
+    private readonly HttpClient _client;
+
+    public RemindersController(IHttpClientFactory factory)
+    {
+        _client = factory.CreateClient("MedicalAPI");
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var response = await _client.GetAsync("api/appointments");
+
+        if (!response.IsSuccessStatusCode)
+            return View(new List<Reminder>());
+
+        var data = await response.Content.ReadAsStringAsync();
+        var appointments = JsonConvert.DeserializeObject<List<Appointment>>(data) ?? new();
+
+        var reminders = appointments
+            .Where(a =>
+                !a.Attended &&
+                a.AppointmentDate > DateTime.Now &&
+                a.AppointmentDate <= DateTime.Now.AddMinutes(30))
+            .Select(a => new Reminder
+            {
+                Id = a.Id,
+                PatientName = a.PatientName,
+                Practitioner = a.Practitioner,
+                ReminderDate = a.AppointmentDate
+            })
+            .ToList();
+
+        return View(reminders);
+    }
+}
